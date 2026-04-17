e261a3ae7a68a80723b973d83dd345390e7ad9ec
shaheedams <mohamedshaheed8055@gmail.com>
2026-04-17 13:27:42 +0530

    CHORE: Base setup

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import { error } from "node:console";
+import app from "./app";
+import { config } from "./config/env";
+
+
+(function () {
+    try {
+        const server = app.listen(config.port, () => {
+            console.log(`Multisite API running`);
+            console.log(`http://localhost:${config.port}/api`);
+            console.log(`Environment: ${config.nodeENV}\n`);
+        });
+
+        const shutDown = (type: string) => {
+            server.close(() => {
+                console.log(`Server is closed, TYPE: ${type}`);
+                process.exit(0);
+            })
+        }
+
+        process.on('SIGINT', () => shutDown("SIGINT"));
+        process.on('SIGTERM', () => shutDown("SIGTERM"));
+
+
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+})()
+
